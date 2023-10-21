4ed42d07fb10fab5e6774b6dafcf2d180bf90ded
alicia3194 <aliciacortinez@gmail.com>
2023-10-21 13:44:58 +0200

    localStr-funcionalidad botones - estilos

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-//Función para cuando se presione el botón de EMPEZAR salga las preguntas
-// Función para traer información desde la API y almacenar en localStorage
+// LLAMAR AL EVENTO DE LOS BOTONES EMPEZAR Y SIGUIENTE
 
 document.addEventListener("DOMContentLoaded", function () {
+  //BOTON EMPEZAR
   const startButton = document.getElementById("startButton");
 
-  async function getQuestionsAndBegin() {
+  //BOTON SIGUIENTE PREGUNTA
+  const nextButton = document.getElementById("nextButton");
+
+  // REGISTRO DE LA PREGUNTA
+  let questionIndex = 0;
+
+  // Función INFORMACIÓN API y GUARDAR EN LOCALST
+  async function getQuestionsApi() {
     try {
       let response = await fetch(
         "https://opentdb.com/api.php?amount=10&type=multiple"
@@ -18,34 +25,89 @@ document.addEventListener("DOMContentLoaded", function () {
         incorrectAnswers: question.incorrect_answers,
       }));
 
+      // Guarda preguntas en el LocalStorage
       localStorage.setItem("questionsData", JSON.stringify(getInfo));
-      showQuestion(getInfo, 0);
+
+      // Mostrar la primera pregunta
+      showQuestion(getInfo, questionIndex);
     } catch (error) {
       console.log(`Hubo un error: ${error}`);
     }
   }
 
-  //Llamar a la funcionalidad de click, esconderse una vez pulsado
+  // OCULTAR BOTÓN EMPEZAR, TITULO Y MENSAJE BIENVENIDA"
   startButton.addEventListener("click", function () {
-    getQuestionsAndBegin();
+    getQuestionsApi();
+    // Ocultar el botón de inicio
     startButton.style.display = "none";
-    // METER ALGO PARA QUE EL TEXTO DE BIENVENIDA SE QUITE TAMBIÉN
+
+    // Ocultar el mensaje de bienvenida
+    const welcomeContainer = document.querySelector("#welcomeContainer");
+    welcomeContainer.style.display = "none";
+
+    // Mostrar el botón "Siguiente Pregunta"
+    nextButton.style.display = "block";
   });
 
-  function showQuestion(questions, index) {
-    let section = document.querySelector("#question_quiz");
-    let question = questions[index];
-    if (question) {
-      let arrTemplateString = `
-        <h1 class="pregunta">${question.question}</h1>
-        <label><input type="radio" value="${question.correctAnswer}" name="res">${question.correctAnswer}</label>
-        <label><input type="radio" value="${question.incorrectAnswers[0]}" name="res">${question.incorrectAnswers[0]}</label>
-        <label><input type="radio" value="${question.incorrectAnswers[1]}" name="res">${question.incorrectAnswers[1]}</label>
-        <label><input type="radio" value="${question.incorrectAnswers[2]}" name="res">${question.incorrectAnswers[2]}</label>
-      `;
-      section.innerHTML = arrTemplateString;
+  // FUNCION PARA MOSTAR LAS PREGUNTAS
+  nextButton.addEventListener("click", function () {
+    // COMPROBAR QUE ALGUNA OPCION HA SIDO SELECCIONADA
+    const typeAnswer = document.querySelectorAll('input[type="radio"]');
+
+    let answerSelected = false;
+    for (let i = 0; i < typeAnswer.length; i++) {
+      if (typeAnswer[i].checked) {
+        answerSelected = true;
+        break;
+      }
+    } // UNA VEZ COMPROBADO Y QUE SEA TRUE REALIZA EL SIGUIENTE CONDICIONAL
+    if (answerSelected) {
+      questionIndex++;
+
+      const questionsData = getQuestionsFromLocalStorage();
+
+      // CONDICIONAL PARA COMPROBAR SI HAY MÁS PREGUNTAS ALMACENADAS
+      if (questionsData && questionIndex < questionsData.length) {
+        showQuestion(questionsData, questionIndex);
+      } else {
+        const section = document.getElementById("question_quiz");
+        section.innerHTML = "¡HAS TERMINADO LAS PREGUNTAS DE QUIZZYWIZ!";
+
+        // OCULTAMOS BOTÓN NEXT
+        nextButton.style.display = "none";
+      }
     } else {
-      section.innerHTML = "No hay más preguntas.";
+      // SI NO HA SIDO SELECCIONADA NINGUNA RESPUESTA
+      alert("Tienes que eleguir alguna respuesta");
     }
+  });
+
+  // MOSTRAR LAS PREGUNTAS Y RESPUESTAS
+  function showQuestion(questions, index) {
+    let section = document.getElementById("question_quiz");
+    let question = questions[index];
+
+    let arrTemplateString = `
+    <h1 class="pregunta">${question.question}</h1>
+    <label><input type="radio" value="${question.correctAnswer}" name="res" required>${question.correctAnswer}</label>
+    <label><input type="radio" value="${question.incorrectAnswers[0]}" name="res" required>${question.incorrectAnswers[0]}</label>
+    <label><input type="radio" value="${question.incorrectAnswers[1]}" name="res" required>${question.incorrectAnswers[1]}</label>
+    <label><input type="radio" value="${question.incorrectAnswers[2]}" name="res" required>${question.incorrectAnswers[2]}</label>
+  `;
+    section.innerHTML = arrTemplateString;
+  }
+
+  // Función para obtener preguntas desde el LocalStorage
+  function getQuestionsFromLocalStorage() {
+    let questionsData = localStorage.getItem("questionsData");
+    if (questionsData) {
+      return JSON.parse(questionsData);
+    }
+    return null;
   }
 });
+
+// *********    FALTA VERIFICAR LAS RESPUESTAS ************ //
+
+// ***** PENSAR EN COMO QUITAR EL ESPACIO QUE SE VE DONDE VAN LAS PREGUNTAS, RESPUESTAS Y BOTON
+//       ANTES DE QUE SE PULSE GO ***** //
