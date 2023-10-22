a0a03a5b62e823b82e94a4dbc51218d0e0eaac4e
Alicia <131009082+alicia3194@users.noreply.github.com>
2023-10-22 16:22:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-
-// LLAMAR AL EVENTO DE LOS BOTONES EMPEZAR Y SIGUIENTE
-
 document.addEventListener("DOMContentLoaded", function () {
-  //BOTON EMPEZAR
+  // BOTON EMPEZAR
   const startButton = document.getElementById("startButton");
 
-  //BOTON SIGUIENTE PREGUNTA
+  // BOTON SIGUIENTE PREGUNTA
   const nextButton = document.getElementById("nextButton");
 
   // REGISTRO DE LA PREGUNTA
   let questionIndex = 0;
+  let score = 0;
 
   // Función INFORMACIÓN API y GUARDAR EN LOCALST
   async function getQuestionsApi() {
@@ -39,18 +37,22 @@ document.addEventListener("DOMContentLoaded", function () {
   // OCULTAR BOTÓN EMPEZAR, TITULO Y MENSAJE BIENVENIDA"
   startButton.addEventListener("click", function () {
     getQuestionsApi();
-    // Ocultar el botón de inicio
+    // Ocultar botón start game
     startButton.style.display = "none";
 
-    // Ocultar el mensaje de bienvenida
+    // OCULTAR MENSAJE BIENVENIDA
     const welcomeContainer = document.querySelector("#welcomeContainer");
     welcomeContainer.style.display = "none";
 
-    // Mostrar el botón "Siguiente Pregunta"
+    // MOSTRAR preguntas y respuestas
+    const questionContainer = document.querySelector("#playQuiz");
+    questionContainer.style.display = "block";
+
+    // MOSTRAR BOTÓN NEXT
     nextButton.style.display = "block";
   });
 
-  // FUNCION PARA MOSTAR LAS PREGUNTAS
+  // FUNCION PARA MOSTRAR LAS PREGUNTAS
   nextButton.addEventListener("click", function () {
     // COMPROBAR QUE ALGUNA OPCION HA SIDO SELECCIONADA
     const typeAnswer = document.querySelectorAll('input[type="radio"]');
@@ -61,7 +63,9 @@ document.addEventListener("DOMContentLoaded", function () {
         answerSelected = true;
         break;
       }
-    } // UNA VEZ COMPROBADO Y QUE SEA TRUE REALIZA EL SIGUIENTE CONDICIONAL
+    }
+
+    // UNA VEZ COMPROBADO Y QUE SEA TRUE REALIZA EL SIGUIENTE CONDICIONAL
     if (answerSelected) {
       questionIndex++;
 
@@ -74,12 +78,12 @@ document.addEventListener("DOMContentLoaded", function () {
         const section = document.getElementById("question_quiz");
         section.innerHTML = "¡HAS TERMINADO LAS PREGUNTAS DE QUIZZYWIZ!";
 
-        // OCULTAMOS BOTÓN NEXT
+        // OCULTAR BOTÓN NEXT
         nextButton.style.display = "none";
       }
     } else {
       // SI NO HA SIDO SELECCIONADA NINGUNA RESPUESTA
-      alert("Tienes que eleguir alguna respuesta");
+      alert("Tienes que elegir alguna respuesta");
     }
   });
 
@@ -97,127 +101,49 @@ document.addEventListener("DOMContentLoaded", function () {
   `;
     section.innerHTML = arrTemplateString;
   }
-
-  // Función para obtener preguntas desde el LocalStorage
-  function getQuestionsFromLocalStorage() {
-    let questionsData = localStorage.getItem("questionsData");
-    if (questionsData) {
-      return JSON.parse(questionsData);
-    }
-    return null;
-  }
 });
+
+function getQuestionsFromLocalStorage() {
+  let questionsData = localStorage.getItem("questionsData");
+  if (questionsData) {
+    return JSON.parse(questionsData);
+  }
+  return null;
+}
+
+// ***** CONSEGUIR DEJAR SELECIONADA UNA RESPUESTA CON EL COLOR ****** //
 
 // *********    FALTA VERIFICAR LAS RESPUESTAS ************ //
 
-// ***** PENSAR EN COMO QUITAR EL ESPACIO QUE SE VE DONDE VAN LAS PREGUNTAS, RESPUESTAS Y BOTON
-//       ANTES DE QUE SE PULSE GO ***** //
-=======
-// FUNCIONALIDAD LOGIN
+// Función para obtener las respuestas seleccionadas por el usuario y las respuestas correctas
+// function getSelectedAnswers() {
+//   const selectedAnswers = [];
+//   const inputs = document.querySelectorAll('input[type="radio"]:checked');
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDlsqX9gEU6ToguhB0yuc1kKTtU93kcBMA",
-  authDomain: "quizii-84a42.firebaseapp.com",
-  projectId: "quizii-84a42",
-  storageBucket: "quizii-84a42.appspot.com",
-  messagingSenderId: "201517071704",
-  appId: "1:201517071704:web:f18386349818ced992f92d",
-};
+//   for (let i = 0; i < inputs.length; i++) {
+//     const input = inputs[i];
+//     const questionIndex = parseInt(input.getAttribute("name").split("_")[1]);
+//     const answer = input.value;
 
-firebase.initializeApp(firebaseConfig);
-
-const createUser = (user) => {
-  db.collection("quizII")
-    .add(user)
-    .then((docRef) => console.log("Document written with ID: ", docRef.id))
-    .catch((error) => console.error("Error adding document: ", error));
-};
-
-const readAllUsers = (born) => {
-  db.collection("quizII")
-    .where("first", "==", born)
-    .get(user)
-    .then((querySnapshot) => {
-      querySnapshot.forEach((user) => {
-        console.log(user.data());
-      });
-    });
-};
-
-const signUpUser = (email, password) => {
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      let user = userCredential.user;
-      alert(`Se ha registrado ${user.email} en el sistema`);
-      createUser({
-        id: user.uid,
-        email: user.email,
-        message: "hola!",
-      });
-    })
-    .catch((error) => {
-      console.log("Error en el sistema" + error.message);
-    });
-};
-
-const signInUser = (email, password) => {
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      let user = userCredential.user;
-      alert(`Se ha logueado correctamente ${user.email}`);
-      console.log("USER", user);
-      window.location.href = "./question.html";
-    })
-    .catch((error) => {
-      let errorCode = error.code;
-      let errorMessage = error.message;
-      console.log(errorCode);
-      console.log(errorMessage);
-      alert(`Error en usuario o contraseña`);
-    });
-};
-
-// document.getElementById("form1").addEventListener("submit", function (event) {
-//   event.preventDefault();
-//   let email = event.target.elements.email.value;
-//   let pass = event.target.elements.pass.value;
-//   let pass2 = event.target.elements.pass2.value;
-
-//   pass === pass2
-//     ? signUpUser(email, pass)
-//     : alert("Las contraseñas no coinciden");
-// });
-
-// const signOut = () => {
-//   let user = firebase.auth().currentUser;
-
-//   firebase
-//     .auth()
-//     .signOut()
-//     .then(() => {
-//       console.log("Sale del sistema: " + user.email);
-//     })
-//     .catch((error) => {
-//       console.log("Hubo un error: " + error);
+//     selectedAnswers.push({
+//       questionIndex: questionIndex,
+//       answer: answer,
 //     });
-// };
+//   }
 
-document.getElementById("form2").addEventListener("submit", function (event) {
-  event.preventDefault();
-  let email = event.target.elements.email2.value;
-  let pass = event.target.elements.pass3.value;
-  signInUser(email, pass);
-});
+//   return selectedAnswers;
+// }
 
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    console.log(`Está en el sistema:${user.email} ${user.uid}`);
-  } else {
-    console.log("No hay usuarios en el sistema");
-  }
-});
+// function showResult() {
+//   let resultsSection = document.querySelector(".results");
+//   let scoresData = JSON.parse(localStorage.getItem("questionsData"));
 
+//   if (scoresData && scoresData.length > 0) {
+//     let lastScore = scoresData[scoresData.length - 1];
+//     let resultTemplate = `<p class='printResult'>${lastScore.score}</p>`;
+//     resultsSection.innerHTML = resultTemplate;
+//   }
+// }
+// ***** CONSEGUIR DEJAR SELECIONADA UNA RESPUESTA CON EL COLOR ****** //
+
+// *********    FALTA VERIFICAR LAS RESPUESTAS ************ //
